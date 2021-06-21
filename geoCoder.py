e020f5539d84d983463174642f8fc5997e988f4f
Daniel Abara <44237659+deehat@users.noreply.github.com>
2021-06-21 19:58:09 +0100

    working locally and on heroku server

--- geoCoder.py
+++ geoCoder.py
@@ -28,24 +28,35 @@ def success():
     if request.method=="POST":
 
         uploaded_file=request.files["csvfile"] # read the csv file which was posted
+        uploaded_file.save(secure_filename(uploaded_file.filename)) # save the file
         df=pd.read_csv(uploaded_file.filename) # read the file to a dataframe
-        
+
         # check that there is a column in the csv with called 'address' or 'Address'
         if checkFileColumns(df) is True:
-            #uploaded_file.save(secure_filename("uploaded"+uploaded_file.filename)) # save the file
+            try:
+                df["Latitude"]=[getLocation(address).latitude for address in df["Address"]]
+            except:
+                df["Latitude"]="Incomplete address"
             
-            df["Latitude"]=[getLocation(address).latitude for address in df["Address"]]
-            df["Longitude"]=[getLocation(address).longitude for address in df["Address"]]
-            
+            try:
+                df["Longitude"]=[getLocation(address).longitude for address in df["Address"]]
+            except:
+                df["Longitude"]="Incomplete address"
             # use exception handling delete the file if it exists otherwise pass
             try:
                 os.remove("geocodedAddress.csv")
             except:
                 pass
+                    
+            df.to_csv("geocodedAddress.csv",index=False) # save dataframe to csv
+            
+            # show only first 5 rows if the no of rows is greater than 5
+            if df.index.max() > 5:
+                tableshown=df.loc[0:4].to_html(classes="tabs",header=True,index=False)
+            else:
+                tableshown=df.to_html(classes="tabs",header=True,index=False)
 
-            df.to_csv("geocodedAddress.csv",index=False) # save df to csv
-
-            return render_template("index.html",btn="download.html",table=df.to_html(classes="tabs",header=True,index=False)) # show the index page but now with the html of download button embedded from download.html
+            return render_template("index.html",btn="download.html",table=tableshown,text2="Only the first 5 rows are shown. Use the button below to download the entire CSV file.") # show the index page but now with the html of download button embedded from download.html
     return render_template("index.html", text="Please ensure that there is an address column in your csv file!")
 
 @app.route("/download")
