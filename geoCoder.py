29a01e95dc0840507e8d59ee8154dd1c0b259127
Daniel Abara <44237659+deehat@users.noreply.github.com>
2021-06-21 16:44:53 +0100

    include deletion of existing csv before saving new one. Clean up code

--- geoCoder.py
+++ geoCoder.py
@@ -2,14 +2,18 @@ from flask import Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
 import pandas as pd
 from geopy.geocoders import ArcGIS
+import os
 
 app=Flask(__name__)
 
+# check if column name address exists
 def checkFileColumns(df):
     if "address" in df.columns or "address".title() in df.columns:
-        return True  
+        return True
+    else:
+        return False  
 
-# function to geocode addresses supplied
+# for geocoding supplied addresses
 def getLocation(address):
     geoLocator=ArcGIS()
     location=geoLocator.geocode(address)
@@ -25,18 +29,21 @@ def success():
 
         uploaded_file=request.files["csvfile"] # read the csv file which was posted
         df=pd.read_csv(uploaded_file.filename) # read the file to a dataframe
-        print(df)
-
+        
         # check that there is a column in the csv with called 'address' or 'Address'
         if checkFileColumns(df) is True:
             #uploaded_file.save(secure_filename("uploaded"+uploaded_file.filename)) # save the file
-            print(uploaded_file)
-            print(uploaded_file.filename) #print the filename
-
+            
             df["Latitude"]=[getLocation(address).latitude for address in df["Address"]]
             df["Longitude"]=[getLocation(address).longitude for address in df["Address"]]
-            print(df)
-            df.to_csv("geocodedAddress.csv",index=False)
+            
+            # use exception handling delete the file if it exists otherwise pass
+            try:
+                os.remove("geocodedAddress.csv")
+            except:
+                pass
+
+            df.to_csv("geocodedAddress.csv",index=False) # save df to csv
 
             return render_template("index.html",btn="download.html",table=df.to_html(classes="tabs",header=True,index=False)) # show the index page but now with the html of download button embedded from download.html
     return render_template("index.html", text="Please ensure that there is an address column in your csv file!")
