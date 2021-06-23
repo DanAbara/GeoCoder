b456e31c83a870b6ebc11963883a66f17b553e12
Daniel Abara <44237659+deehat@users.noreply.github.com>
2021-06-23 19:45:02 +0100

    Clean up

--- geoCoder.py
+++ geoCoder.py
@@ -1,3 +1,8 @@
+""" 
+Date: 19/06/2021
+Author: DA  
+
+"""
 from flask import Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
 import pandas as pd
@@ -27,11 +32,12 @@ def index():
 def success():
     if request.method=="POST":
 
-        uploaded_file=request.files["csvfile"] # read the csv file which was posted
-        uploaded_file.save(secure_filename(uploaded_file.filename)) # save the file
-        df=pd.read_csv(uploaded_file.filename) # read the file to a dataframe
+        # read and save uploaded csv and convert to dataframe
+        uploaded_file=request.files["csvfile"] 
+        uploaded_file.save(secure_filename(uploaded_file.filename)) 
+        df=pd.read_csv(uploaded_file.filename) 
 
-        # check that there is a column in the csv with called 'address' or 'Address'
+        # create and populate lon/lat columns if address column exists
         if checkFileColumns(df) is True:
             try:
                 df["Latitude"]=[getLocation(address).latitude for address in df["Address"]]
@@ -42,21 +48,28 @@ def success():
                 df["Longitude"]=[getLocation(address).longitude for address in df["Address"]]
             except:
                 df["Longitude"]="Incomplete address"
-            # use exception handling delete the file if it exists otherwise pass
+
+            # delete file from any previous geocoding task
             try:
                 os.remove("geocodedAddress.csv")
             except:
                 pass
-                    
-            df.to_csv("geocodedAddress.csv",index=False) # save dataframe to csv
+
+            # save dataframe to csv       
+            df.to_csv("geocodedAddress.csv",index=False) 
             
             # show only first 5 rows if the no of rows is greater than 5
             if df.index.max() > 5:
                 tableshown=df.loc[0:4].to_html(classes="tabs",header=True,index=False)
             else:
                 tableshown=df.to_html(classes="tabs",header=True,index=False)
-
-            return render_template("index.html",btn="download.html",table=tableshown,text2="Only the first 5 rows are shown. Use the button below to download the entire CSV file.") # show the index page but now with the html of download button embedded from download.html
+            
+            # return index page with download button and text
+            return render_template("index.html",
+                                    btn="download.html",
+                                    table=tableshown,
+                                    text2="Only the first 5 rows are shown. Use the button below to download the entire CSV file.") 
+    
     return render_template("index.html", text="Please ensure that there is an address column in your csv file!")
 
 @app.route("/download")
